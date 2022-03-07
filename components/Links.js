fbba846a3e1ee4040f4413e995277682adf92a83
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2022-03-07 14:08:26 -0300

    Update Links.js

--- components/Links.js
+++ components/Links.js
@@ -10,13 +10,13 @@ export default function Links(){
                     <a href="https://trinoonline.com.br/" className="text-white bg-purple-800 hover:bg-purple-400  dark:bg-orange-600 dark:hover:bg-orange-400 transition-all w-btnmd h-btnsm rounded-md">
                         Acesse o nosso site
                     </a>
-                    <a href="#" className="text-white bg-purple-800 hover:bg-purple-400  dark:bg-orange-600 dark:hover:bg-orange-400 transition-all w-btnmd h-btnsm rounded-md">
+                    <a href="https://wa.me/5528992729289?text=Olá!+Vim+do+Instagram.+Quero+mais+informações+sobre+o+plano." className="text-white bg-purple-800 hover:bg-purple-400  dark:bg-orange-600 dark:hover:bg-orange-400 transition-all w-btnmd h-btnsm rounded-md">
                         WhatsApp
                     </a>
                     <a href="https://play.google.com/store/apps/details?id=br.com.spsnet.trino&hl=pt_BR&gl=US" className="text-white bg-purple-800 hover:bg-purple-400  dark:bg-orange-600 dark:hover:bg-orange-400 transition-all w-btnmd h-btnsm rounded-md">
                         Baixe nosso App <br/>na Play Store
                     </a>
-                    <a href="#" className="text-white bg-purple-800 hover:bg-purple-400  dark:bg-orange-600 dark:hover:bg-orange-400 transition-all w-btnmd h-btnsm rounded-md">
+                    <a href="https://apps.apple.com/br/app/trino-sa%C3%BAde/id1609767214" className="text-white bg-purple-800 hover:bg-purple-400  dark:bg-orange-600 dark:hover:bg-orange-400 transition-all w-btnmd h-btnsm rounded-md">
                        Baixe nosso App <br/>na AppStore
                     </a>
                     <LogoHospitais/>
